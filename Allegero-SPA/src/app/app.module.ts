c5d2f0ee8fa94f3df6e7af6b5bcdad71f7fd4a88
din000 <66588516+din000@users.noreply.github.com>
2020-07-25 13:46:11 +0200

    font awesome & bootstrap

--- Allegero-SPA/src/app/app.module.ts
+++ Allegero-SPA/src/app/app.module.ts
@@ -4,20 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthService } from './_services/auth.service';
+import { NavComponent } from './nav/nav.component';
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
-  providers: [
-    AuthService
-  ],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [
+      AuthService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
