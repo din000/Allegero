ad155d31390d9faeec4b23bfa86902abd3071a78
din000 <66588516+din000@users.noreply.github.com>
2020-08-06 10:50:21 +0200

    flex home

--- Allegero-SPA/src/app/nav/nav.component.ts
+++ Allegero-SPA/src/app/nav/nav.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
+import { Directive, Input, Output, EventEmitter, OnChanges, OnDestroy } from '@angular/core';
+import { Subject, Observable, SubscriptionLike, timer } from 'rxjs';
+import { switchMap, take, tap } from 'rxjs/operators';
+
+
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+
+export class NavComponent implements OnInit{
 
   modalRef: BsModalRef; // do loginu i rejestracji
 
@@ -18,9 +25,17 @@ export class NavComponent implements OnInit {
             '../../assets/laptop2.jpg',
             '../../assets/laptop3.jpg'];
 
-  // do loginu i rejestracji
-  constructor(private modalService: BsModalService) { }
+  // do minutnika
+  interval; // domyslnie jest 6, dziala dobrze XD
+  secondsLeft = 60;
+  minLeft = 59;
+  hoursLeft = 23;
 
+
+  // do loginu i rejestracji private modalService: BsModalService
+  constructor(private modalService: BsModalService) {}
+
+    // do slajderow
     customOptions: OwlOptions = {
       loop: true,
       mouseDrag: true,
@@ -30,24 +45,17 @@ export class NavComponent implements OnInit {
       navSpeed: 700,
       navText: ['', ''],
       responsive: {
-        0: {
-          items: 1
-        },
-        1: {
-          items: 2
-        },
-        2: {
-          items: 3
-        },
-        3: {
-          items: 4
-        },
+        0: {items: 1},
+        1: {items: 2},
+        2: {items: 3},
+        3: {items: 4},
       },
       nav: false
     };
 
 
   ngOnInit() {
+    this.startTimer();
   }
 
   // do loginu i rejestracji
@@ -57,5 +65,33 @@ export class NavComponent implements OnInit {
   // do loginu i rejestracji
   openModalRegister(template2: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template2);
+  }
+
+  // do minitnika
+  startTimer() {
+      this.interval = setInterval(() => {
+        if (this.hoursLeft >= 0 && this.minLeft >= 0 && this.secondsLeft >= 0){
+          if (this.hoursLeft > 0 && this.minLeft === 0){
+            this.minLeft = 59; // 59 bo petla robi sie gdy jest 0 i traci sie 1 sekunde
+            if (this.hoursLeft > 0){
+              this.hoursLeft --;
+            }
+          }
+          if (this.secondsLeft > 0){
+            this.secondsLeft --;
+          }
+          else if (this.minLeft > 0 && this.secondsLeft === 0){
+            this.secondsLeft = 59; // 59 bo petla robi sie gdy jest 0 i traci sie 1 sekunde
+            if (this.minLeft > 0){
+              this.minLeft --;
+            }
+          }
+        }
+      }, 1000);
+    }
+
+  // do minutnika
+  pauseTimer() {
+    clearInterval(this.interval);
   }
 }
